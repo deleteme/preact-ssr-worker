98f08d335d42365eb5c596d7a07291e25e5229d1
Matt Garrett <mattga@gmail.com>
2021-08-20 18:20:55 -0700

    copy to public

--- public/src/app.js
+++ public/src/app.js
@@ -1,14 +1,26 @@
-import { useState } from 'preact/hooks'
+import { useState, useEffect, useRef } from 'preact/hooks'
 import { html } from './html.js'
 import { Subscriptions } from './subscriptions.js'
+import { useQuery, CollectionContext, gql } from './experiment-with-context.js'
 
-export function App(props = {}) {
-  console.log('App() called with props', JSON.stringify(props))
+const Layout = props => {
   const [value, setValue] = useState(0)
-  const { params, queryResult } = props
-  //console.log('params', params);
+  const { params } = props
   const page = params && params.page
-  const organization = queryResult.data.organization
+  const orgResult = useQuery(
+    gql`
+      query Organization($id: ID!) {
+        organization(organizationId: $id) {
+          id
+          name
+        }
+      }
+    `,
+    { variables: { id: 'fcbf1994-4b35-451d-bffb-90cb6032f42b' } },
+  )
+  const organization = orgResult.data && orgResult.data.organization
+  console.log('org orgResult', orgResult)
+  if (!organization) return ``
   return html`
     <header>
       <h1>
@@ -28,7 +40,7 @@ export function App(props = {}) {
       </h1>
       ${page === 'subscriptions' &&
         Subscriptions({
-          queryResult,
+          orgId: organization.id,
         })}
     </main>
     <footer>
@@ -39,5 +51,15 @@ export function App(props = {}) {
         toggle
       </button>
     </footer>
+  `
+}
+
+export function App(props = {}) {
+  console.log('\nApp() called with props', JSON.stringify(props))
+  const { collection } = props
+  return html`
+    <${CollectionContext.Provider} value=${collection}>
+      <${Layout} ...${props}><//>
+    <//>
   `
 }
--- public/src/subscriptions.js
+++ public/src/subscriptions.js
@@ -1,11 +1,49 @@
 import { html } from './html.js'
+import { gql, useQuery } from './experiment-with-context.js'
 
 export function Subscriptions(props) {
-  const subscriptions =
-    props.queryResult.data.organization.subscriptionProducts.nodes
+  const orgId = props.orgId
+
+  const subscriptionResult = useQuery(
+    gql`
+      query OrgHomeSubscriptions($orgId: ID!) {
+        organization(organizationId: $orgId) {
+          id
+          subscriptionProducts: products(
+            where: {
+              published: { equals: true }
+              listed: { equals: true }
+              type: { key: { equals: "subscription" } }
+            }
+          ) {
+            nodes {
+              id
+            }
+            totalCount
+          }
+        }
+      }
+    `,
+    {
+      variables: { orgId },
+    },
+  )
+  console.log('subscriptionResult', JSON.stringify(subscriptionResult))
+  const subscriptionsProducts =
+    subscriptionResult.data &&
+    subscriptionResult.data.organization.subscriptionProducts
+  if (!subscriptionsProducts)
+    return html`
+      <em>loading subs</em>
+    `
+
   return html`
     <section>
       <em>subscription ids</em>
+      <pre>
+      totalCount: ${subscriptionsProducts.totalCount}
+    </pre
+      >
       <table>
         <thead>
           <tr>
@@ -13,7 +51,7 @@ export function Subscriptions(props) {
           </tr>
         </thead>
         <tbody>
-          ${subscriptions.map(subscription => {
+          ${subscriptionsProducts.nodes.map(subscription => {
             return html`
               <tr>
                 <td>${subscription.id}</td>
