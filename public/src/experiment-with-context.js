96dbc577e0bed34877102af8369241c71fb47205
Matt Garrett <mattga@gmail.com>
2021-08-23 09:34:05 -0700

    copy to public

--- public/src/experiment-with-context.js
+++ public/src/experiment-with-context.js
@@ -17,31 +17,6 @@ class Collection {
     if (idRef.current === null) {
       console.log('idRef.current is null. assigning an id')
       idRef.current = this.getNextId()
-      /*
-      const isFresh = this.processed.size === 0;
-      // 1st pass. starting from scratch. make new ids.
-      if (isFresh) {
-        idRef.current = this.getNextId();
-      } else {
-        // has processed queries
-
-        either the id is a processed id
-          and needs to be restored
-          idRef.current = 0
-        or it's a new one
-          idRef.current = this.getNextId();
-
-
-        //const hasNewPendingQueries = this.pending.size > 0;
-        //if (hasNewPendingQueries) {
-        //} else {
-        //}
-        // 1 = 1 - 0
-        //
-        idRef.current = this.processed.size - this.pending.size;
-        // Are any pending
-      }
-      */
     }
     const id = idRef.current
     console.log(
@@ -56,7 +31,6 @@ class Collection {
       return this.processed.get(id)
     } else {
       this.ids.push(id)
-      console.log('added idRef to set', this.ids)
       this.pending.set(id, item)
       return item
     }
@@ -74,7 +48,6 @@ class Collection {
       this.lastProvisionedId += 1
     }
     return this.lastProvisionedId
-    //return this.ids.length
   }
   async process() {
     console.log('processing started')
@@ -89,6 +62,38 @@ class Collection {
       }
     }
     console.log('processing ending')
+  }
+  toString() {
+    const processed = Array.from(this.processed);
+    const pending = Array.from(this.pending);
+    return JSON.stringify({
+      lastProvisionedId: this.lastProvisionedId,
+      ids: this.ids,
+      processed,
+      pending
+    })
+  }
+  restore(parsed) {
+    console.log('collection.restore called with', parsed);
+    //const parsed = JSON.parse(string);
+    this.lastProvisionedId = parsed.lastProvisionedId;
+    this.ids = parsed.ids;
+
+    const makeQuery = q => {
+      const query = new Query(q.query, {
+        operationName: q.operationName,
+        variables: q.variables
+      });
+      query.restore(q);
+      return query;
+    };
+
+    this.pending = new Map(parsed.pending.map((q, id) => {
+      return [id, makeQuery(q)];
+    }));
+    this.processed = new Map(parsed.processed.map((q, id) => {
+      return [id, makeQuery(q)];
+    }));
   }
 }
 const PENDING = 'PENDING'
@@ -132,6 +137,28 @@ class Query {
     }
     return this
   }
+  toString() {
+    const { status, data, error, query, operationName, variables } = this;
+    return JSON.stringify({ status, data, error, query, operationName, variables });
+  }
+  restore(parsed) {
+    console.log('Query.restore() called with', parsed);
+    this.status = parsed.status;
+    this.data = parsed.data;
+    this.error = parsed.error
+    this.query = parsed.query;
+    this.operationName = parsed.operationName;
+    this.variables = parsed.variables;
+  }
+  //fromString(string) {
+    //const parsed = JSON.parse(string);
+    //this.status = parsed.status;
+    //this.data = parsed.data;
+    //this.error = parsed.error
+    //this.query = parsed.query;
+    //this.operationName = parsed.operationName;
+    //this.variables = parsed.variables;
+  //}
 }
 
 export const collection = new Collection()
