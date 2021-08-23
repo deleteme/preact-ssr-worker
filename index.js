3ca8ee892e6d3a8c5d853d0070e068902d598b54
Matt Garrett <mattga@gmail.com>
2021-08-23 15:59:09 -0700

    format

--- index.js
+++ index.js
@@ -50,7 +50,9 @@ const doc = ({ children, appProps, renderMeasurements }) => {
           ${children}
         </div>
         <script type="module">
-          const renderMeasurements = JSON.parse("${JSON.stringify(renderMeasurements)}")
+          const renderMeasurements = JSON.parse("${JSON.stringify(
+            renderMeasurements,
+          )}")
           console.log("renderMeasurements", renderMeasurements);
         </script>
       </body>
@@ -60,20 +62,20 @@ const doc = ({ children, appProps, renderMeasurements }) => {
 
 const renderAndRespond = async ({ params = {} }) => {
   const appProps = { params, collection }
-  const renderMeasurements = [];
+  const renderMeasurements = []
 
   let renderCount = 0
   let renderedApp = ''
 
   const doRender = () => {
-    const start = Date.now();
+    const start = Date.now()
     renderCount += 1
     console.log('\n'.repeat(4))
     console.log('RENDER', renderCount, 'started')
     renderedApp = render(h(App, appProps), {}, { pretty: true })
     console.log('RENDER', renderCount, 'completed')
-    const end = Date.now();
-    renderMeasurements.push(end - start);
+    const end = Date.now()
+    renderMeasurements.push(end - start)
   }
   doRender()
 
@@ -87,10 +89,10 @@ const renderAndRespond = async ({ params = {} }) => {
   const body = doc({
     appProps,
     children: renderedApp,
-    renderMeasurements
+    renderMeasurements,
   })
 
-  collection.reset();
+  collection.reset()
 
   return new Response(body, {
     headers: { 'content-type': 'text/html' },
